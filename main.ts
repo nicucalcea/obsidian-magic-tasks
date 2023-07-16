d1ed604b5713906cfd45f70d0c1cc11f79956694
nicucalcea <mail@nicu.md>
2023-07-16 22:06:53 +0100

    working version-ish

--- main.ts
+++ main.ts
@@ -1,137 +1,89 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin, Editor } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  private activeEditor: Editor | null = null;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    this.registerDomEvent(document, 'DOMNodeInserted', this.handleDomMutation);
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  handleDomMutation = (event: MutationEvent) => {
+    if (!(event.target instanceof HTMLElement)) return;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // Check if the inserted node has the suggestion-container class
+    if (event.target.classList.contains('suggestion-container')) {
+      this.appendSuggestionItem(event.target);
+    }
+  };
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  appendSuggestionItem(container: HTMLElement) {
+    // Create a new suggestion item element
+    const suggestionItem = document.createElement('div');
+    suggestionItem.className = 'suggestion-item';
+    suggestionItem.textContent = 'New Suggestion Item';
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    // Add a click event listener to the suggestion item
+    suggestionItem.addEventListener('click', async () => {
+      await this.handleSuggestionItemSelected();
+    });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    // Append the suggestion item to the suggestion container
+    const suggestionContainer = container.querySelector('.suggestion');
+    if (suggestionContainer) {
+      suggestionContainer.appendChild(suggestionItem);
+    }
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  async handleSuggestionItemSelected() {
+    console.log('Suggestion item selected!');
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    // Get the text contents of a div with the class `.cm-active`
+    const activeDiv = document.querySelector('.cm-active');
+    const activeText = activeDiv ? activeDiv.textContent : '';
 
-	onunload() {
+    // Make API request to OpenAI to get subtasks breakdown
+    try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer sk-XXX', // Replace with your actual API key
+        },
+        body: JSON.stringify({
+          "model": "gpt-3.5-turbo",
+          "messages": [
+            {"role": "system", "content": "You are a helpful assistant that responds with markdown-formatted tasks in the format `- [ ] This is a task`. Please only respond with tasks, no other text."},
+            {"role": "user", "content": `Create sub-tasks for this task: ${activeText}`} // Append activeText to the user message
+          ]
+        }),
+      });
 
-	}
+      if (!response.ok) {
+        console.error('Error sending request to OpenAI API');
+        return;
+      }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+      const data = await response.json();
+      console.log(data);
+      const subtasks = data.choices[0].message.content;
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+      console.log('Subtasks breakdown:');
+      console.log(subtasks);
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+      // Append the subtasks to the active editor
+      if (this.activeEditor) {
+        this.activeEditor.replaceSelection('\n\n' + subtasks);
+      }
+    } catch (error) {
+      console.error('Error fetching data from OpenAI API:', error);
+    }
+  }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  onunload() {
+    this.activeEditor = null;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  async onEditorChange(editor: Editor) {
+    this.activeEditor = editor;
+  }
 }
