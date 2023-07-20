bf5804dda3a09c8da7a30c5b9b45605d239f8c4e
nicucalcea <mail@nicu.md>
2023-07-20 22:35:36 +0100

    this actually works

--- main.ts
+++ main.ts
@@ -1,89 +1,75 @@
-import { Plugin, Editor } from 'obsidian';
+import { Plugin, Editor, MarkdownView } from 'obsidian';
 
-export default class MyPlugin extends Plugin {
-  private activeEditor: Editor | null = null;
+// Modify this with your actual OpenAI API endpoint URL
+const OPENAI_API_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
+const OPENAI_API_KEY = '';
 
-  async onload() {
-    this.registerDomEvent(document, 'DOMNodeInserted', this.handleDomMutation);
-  }
+// selects the entire line from index zero
+function selectLine(editor: Editor) {
+    const { line } = editor.getCursor();
+    const lineText = editor.getLine(line);
+    editor.setSelection({ line, ch: 0 }, { line, ch: lineText.length });
+}
 
-  handleDomMutation = (event: MutationEvent) => {
-    if (!(event.target instanceof HTMLElement)) return;
-
-    // Check if the inserted node has the suggestion-container class
-    if (event.target.classList.contains('suggestion-container')) {
-      this.appendSuggestionItem(event.target);
+export default class MagicTasksPlugin extends Plugin {
+    async onload() {
+        // This adds an editor command on the current editor instance
+        this.addCommand({
+            id: 'select-current-line-on-keystroke',
+            name: 'select the current line in editor',
+            hotkeys: [{ modifiers: [], key: 'Escape' }],
+            editorCallback: async (editor: Editor) => {
+                selectLine(editor);
+                const selectedText = editor.getSelection();
+                // console.log('Selected Text:', selectedText);
+                const subtasks = await this.sendToOpenAI(selectedText);
+                this.insertIndentedSubtasks(editor, subtasks);
+            },
+        });
     }
-  };
 
-  appendSuggestionItem(container: HTMLElement) {
-    // Create a new suggestion item element
-    const suggestionItem = document.createElement('div');
-    suggestionItem.className = 'suggestion-item';
-    suggestionItem.textContent = 'New Suggestion Item';
+    insertIndentedSubtasks(editor: Editor, subtasks: string) {
+        const indentedSubtasks = subtasks
+            .split('\n')
+            .map((line) => `\t${line}`)
+            .join('\n');
 
-    // Add a click event listener to the suggestion item
-    suggestionItem.addEventListener('click', async () => {
-      await this.handleSuggestionItemSelected();
-    });
-
-    // Append the suggestion item to the suggestion container
-    const suggestionContainer = container.querySelector('.suggestion');
-    if (suggestionContainer) {
-      suggestionContainer.appendChild(suggestionItem);
+        const { line } = editor.getCursor();
+        editor.replaceRange(`\n${indentedSubtasks}`, { line: line + 1, ch: 0 });
     }
-  }
 
-  async handleSuggestionItemSelected() {
-    console.log('Suggestion item selected!');
+    async sendToOpenAI(text: string): Promise<string> {
+        try {
+            // console.log('Sending to OpenAI:', text);
+            const response = await fetch(OPENAI_API_ENDPOINT, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${OPENAI_API_KEY}`,
+                },
+                body: JSON.stringify({
+                    "model": "gpt-3.5-turbo",
+                    "messages": [
+                        { "role": "system", "content": "You are a helpful assistant that responds with markdown-formatted subttasks in the format `- [ ] This is a subtask`. Please only respond with tasks, no other text." },
+                        { "role": "user", "content": `Please break down the following task into smaller subtasks, and indent them with a \t: ${text}` } // Append selectedText to the user message
+                    ]
+                }),
+            });
 
-    // Get the text contents of a div with the class `.cm-active`
-    const activeDiv = document.querySelector('.cm-active');
-    const activeText = activeDiv ? activeDiv.textContent : '';
+            if (!response.ok) {
+                console.error('Error sending request to OpenAI API');
+                return '';
+            }
 
-    // Make API request to OpenAI to get subtasks breakdown
-    try {
-      const response = await fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer sk-XXX', // Replace with your actual API key
-        },
-        body: JSON.stringify({
-          "model": "gpt-3.5-turbo",
-          "messages": [
-            {"role": "system", "content": "You are a helpful assistant that responds with markdown-formatted tasks in the format `- [ ] This is a task`. Please only respond with tasks, no other text."},
-            {"role": "user", "content": `Create sub-tasks for this task: ${activeText}`} // Append activeText to the user message
-          ]
-        }),
-      });
-
-      if (!response.ok) {
-        console.error('Error sending request to OpenAI API');
-        return;
-      }
-
-      const data = await response.json();
-      console.log(data);
-      const subtasks = data.choices[0].message.content;
-
-      console.log('Subtasks breakdown:');
-      console.log(subtasks);
-
-      // Append the subtasks to the active editor
-      if (this.activeEditor) {
-        this.activeEditor.replaceSelection('\n\n' + subtasks);
-      }
-    } catch (error) {
-      console.error('Error fetching data from OpenAI API:', error);
+            const data = await response.json();
+            const subtasks = data.choices[0].message.content;
+            // console.log('OpenAI data:', subtasks);
+            return subtasks;
+        } catch (error) {
+            // console.error('Error while calling OpenAI API:', error);
+            return '';
+        }
     }
-  }
 
-  onunload() {
-    this.activeEditor = null;
-  }
-
-  async onEditorChange(editor: Editor) {
-    this.activeEditor = editor;
-  }
+    onunload() { }
 }
